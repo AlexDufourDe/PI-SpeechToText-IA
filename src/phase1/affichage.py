4d136f57f1424ca33fc34abebee550a45f72226a
Elisa Bonifas <elisa.bonifas@telecomnancy.eu>
2023-01-09 11:13:16 +0100

    delete phase 1

--- src/phase1/affichage.py
+++
@@ -1,6 +0,0 @@
-def affichage(tab):
-    print("\n Tableau de comparaison")
-    print("________________________________")
-    for (a,b,c) in tab: 
-        print("|   "+a+"  --->  "+b+"      : "+c)
-    print("________________________________")
