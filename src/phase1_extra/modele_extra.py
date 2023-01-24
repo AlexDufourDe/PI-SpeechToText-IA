f0eb45413506ef48ef93ecbf91f2e8f9994dca57
Elisa Bonifas <elisa.bonifas@telecomnancy.eu>
2023-01-24 14:11:08 +0100

    modification passage en argument

--- src/phase1_extra/modele_extra.py
+++ src/phase1_extra/modele_extra.py
@@ -5,7 +5,7 @@ Pour enregistrer le modèle entrainé, il suffit de décommenter les 3 dernière
 This file defined and  train the model using the pre-processed data on 30 words.
 To save the model, you have to uncomment the last three ligns.
 """
-
+import argparse
 import numpy as np
 import tensorflow as tf
 import os
@@ -16,82 +16,87 @@ from pretraitement_extra import download_data
 
 DATA="hub://activeloop/speech-commands-train"
 NB_MOTS = 30 # Changer en fonction du nombre de mots du corpus
-
 CHEMIN_SAUVEGARDE_MODELE = './src/phase1_extra/modeles_extra' # Dossier de sauvegarde des modèles entrainés
 
-len_arg = len(sys.argv)
-if (len_arg>4):
-      print(f"Too many argument, expected :3 , got {len_arg}")
-elif len_arg==2: #(NOM_MODELE)
-      NOM_MODELE=sys.argv[1]
 
-elif len_arg==3:   #(NOM_MODELE)  (NOMBRE_EPOCH) 
-      NOM_MODELE=sys.argv[1]
-      NB_EPOCH=sys.argv[2]
+# Command line parser
 
-elif len_arg==4:   #(NOM_MODELE)  (NOMBRE_EPOCH) (REPERTOIRE_DONNEES)
-      NOM_MODELE=sys.argv[1]
-      NB_EPOCH=int(sys.argv[2])
-      CHEMIN_DONNEES=sys.argv[3]
+parser = argparse.ArgumentParser(description="Test the transcription of a wav file with a CNN model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-p", "--path",  help="Path to the folder containing the data, the default value is './src/phase1_extra/donnees_traitees_extra'")
+parser.add_argument("-n","--name",help="name of the model to train, by default it is 'mel-cnn-enhance' " )
+parser.add_argument("-e","--epochs",default=1,help="Number of epochs for training, by default it is 8")
 
+args = vars(parser.parse_args())
+
+
+if (args['path']):
+    CHEMIN_DONNEES = args['path'] 
+else:   
+    CHEMIN_DONNEES= '.src/phase1/donnees_traitees' # Dossier contenant les données pré-traitées
+
+if (args['name']):
+    NOM_MODELE = args['name']
 else: 
-    CHEMIN_DONNEES= './src/phase1_extra/donnees_traitees_extra' # Dossier contenant les données pré-traitées
-    NOM_MODEL='mel-cnn-enhance'
-    NB_EPOCH=15
+    NOM_MODEL='mel-cnn'
 
-if (len_arg<=4):
-        
-    if not os.path.exists(CHEMIN_DONNEES):
-        download_data(DATA,CHEMIN_DONNEES)
+if (args['epochs']):
+    NB_EPOCH = args['epochs']
+else: 
+    NB_EPOCH=8
 
-
-    # Importation des données
-    x_train, y_train = np.load(CHEMIN_DONNEES+'/train_data.npy')[...,np.newaxis], np.load(CHEMIN_DONNEES+'/train_labels.npy')
-    x_test, y_test = np.load(CHEMIN_DONNEES+'/test_data.npy')[...,np.newaxis], np.load(CHEMIN_DONNEES+'/test_labels.npy')
+    
+if not os.path.exists(CHEMIN_DONNEES):
+    download_data(DATA,CHEMIN_DONNEES)
 
 
-    # Architecture du modèle
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv2D(32, 3, strides=2, padding='same', activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Conv2D(128, 3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(256, activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dropout(0.5))
-    model.add(tf.keras.layers.Dense(NB_MOTS, activation='softmax'))
+# Importation des données
+x_train, y_train = np.load(CHEMIN_DONNEES+'/train_data.npy')[...,np.newaxis], np.load(CHEMIN_DONNEES+'/train_labels.npy')
+x_test, y_test = np.load(CHEMIN_DONNEES+'/test_data.npy')[...,np.newaxis], np.load(CHEMIN_DONNEES+'/test_labels.npy')
 
-    # Compilation du modèle
-    model.compile(
-        loss='sparse_categorical_crossentropy',
-        optimizer=tf.keras.optimizers.RMSprop(),
-        metrics=['accuracy'],
-    )
 
-    # Entrainement du modèle
-    history = model.fit(x_train, y_train, validation_split= 0.15, epochs=NB_EPOCH)
+# Architecture du modèle
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, 3, strides=2, padding='same', activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(128, 3, padding='same', activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(NB_MOTS, activation='softmax'))
 
-    # Evaluation du modèle
-    acc=model.evaluate(x_test, y_test)
+# Compilation du modèle
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.RMSprop(),
+    metrics=['accuracy'],
+)
 
-    version = open("src/phase1_extra/version_model_extra.txt", "a")
-    version.write("\n")
-    version.write(str(datetime.datetime.today()))
-    version.write("  "+NOM_MODEL+ " entrainé sur "+CHEMIN_DONNEES+"\n")
-    version.write("loss : "+str(acc[0])+", accuracy : "+str(acc[1]))
-    version.write("epoch :"+ str(NB_EPOCH))
-    version.close()
+# Entrainement du modèle
+history = model.fit(x_train, y_train, validation_split= 0.15, epochs=NB_EPOCH)
 
-    # Sauvegarde du modèle entraîné (decommenter la ligne si le modèle est satisfaisant)
-    if not os.path.exists(CHEMIN_SAUVEGARDE_MODELE):
-        os.makedirs(CHEMIN_SAUVEGARDE_MODELE)
-    model.save(CHEMIN_SAUVEGARDE_MODELE+"/"+NOM_MODEL)
+# Evaluation du modèle
+acc=model.evaluate(x_test, y_test)
+
+version = open("src/phase1_extra/version_model_extra.txt", "a")
+version.write("\n")
+version.write(str(datetime.datetime.today()))
+version.write("  "+NOM_MODEL+ " entrainé sur "+CHEMIN_DONNEES+"\n")
+version.write("loss : "+str(acc[0])+", accuracy : "+str(acc[1]))
+version.write("epoch :"+ str(NB_EPOCH))
+version.close()
+
+# Sauvegarde du modèle entraîné (decommenter la ligne si le modèle est satisfaisant)
+if not os.path.exists(CHEMIN_SAUVEGARDE_MODELE):
+    os.makedirs(CHEMIN_SAUVEGARDE_MODELE)
+model.save(CHEMIN_SAUVEGARDE_MODELE+"/"+NOM_MODEL)
--- src/phase1_extra/test_fichier_extra.py
+++ src/phase1_extra/test_fichier_extra.py
@@ -14,52 +14,67 @@ import tensorflow_io as tfio
 import os
 from scipy.io import wavfile
 import sys
+import argparse
 
 
 MOTS = ['Sheila','Zero','Go','Bed','Bird','Stop','Marvin','Yes','Four','House','Off','Tree','Wow','Happy','Nine','Up','Three','Right','Five','Two','One','Left','Eight','Six','Down','Dog','No','Cat','On','Seven']
 
-#Path to file
-len_arg = len(sys.argv)
-if (len_arg>3):
-      print(f"Too many argument, expected : 2 , got {len_arg}")
-elif len_arg==1:
-      print(f"Not enough argument, expected : 2 , got {len_arg}")
-elif len_arg==3:
-      CHEMIN_MODELE=sys.argv[2]
-      f_name=sys.argv[1]
+
+# Command line parser
+
+parser = argparse.ArgumentParser(description="Test the transcription of a wav file with a CNN model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-p", "--path",  help="Path to the folder containing the .wav files")
+parser.add_argument("-m","--model",help="Path to the folder of the model" )
+
+args = vars(parser.parse_args())
+f_name=args['path']
+
+
+
+if (not f_name):
+      print(f"Not enough argument, you have to specify the path to the file using -p")
+      exit()
+
+if(args['model']):
+      CHEMIN_MODELE=args['model']
 else: 
       CHEMIN_MODELE = './src/phase1_extra/modeles_extra/mel-cnn-enhance'  
-      f_name=sys.argv[1]
+
 
 if not os.path.exists(CHEMIN_MODELE):
       print(f"The model {CHEMIN_MODELE} does not exist")
-else:
-      if not os.path.exists(f_name):
-            print(f"The file {f_name} does not exist")
-      else:
+      exit()
 
-            samplerate, data = wavfile.read(f_name)
+if not os.path.exists(f_name):
+      print(f"The file {f_name} does not exist")
+      exit()
 
-
-            # Importation du mpdèle entrainé
-            model = tf.keras.models.load_model(CHEMIN_MODELE)
+#Path to file
 
 
-            scaled = np.int16(data* 32767).reshape(16000) # Remise à l'echelle de l'audio
+samplerate, data = wavfile.read(f_name)
 
-            # Ici, on réapplique le mêmes pré-traitements que pour les données d'entraînements.
-            fade = tfio.audio.fade(scaled, fade_in=1000, fade_out=2000, mode="logarithmic")
-            spectrogram = tfio.audio.spectrogram(fade, nfft=1024, window=1024, stride=256)
-            mel_spectrogram = tfio.audio.melscale(spectrogram, rate=16000, mels=128, fmin=0, fmax=8000)
 
-            test = np.array(tfio.audio.dbscale(mel_spectrogram, top_db=80))[...,np.newaxis]
+# Importation du mpdèle entrainé
+model = tf.keras.models.load_model(CHEMIN_MODELE)
 
-            # Prediction
-            index = np.argmax(model.predict(np.array([test])))
 
-            print("\n")
-            print("\nLe mot retranscrit est ---> " + (MOTS[index]).upper() + " <---")
-            print("Si ce n'est pas la bonne retranscription, veuillez revérifier le fichier audio, l'erreur vient probablement de "
-                  "l'enregistrement ")
-            print("\n")
+scaled = np.int16(data* 32767).reshape(16000) # Remise à l'echelle de l'audio
+
+# Ici, on réapplique le mêmes pré-traitements que pour les données d'entraînements.
+fade = tfio.audio.fade(scaled, fade_in=1000, fade_out=2000, mode="logarithmic")
+spectrogram = tfio.audio.spectrogram(fade, nfft=1024, window=1024, stride=256)
+mel_spectrogram = tfio.audio.melscale(spectrogram, rate=16000, mels=128, fmin=0, fmax=8000)
+
+test = np.array(tfio.audio.dbscale(mel_spectrogram, top_db=80))[...,np.newaxis]
+
+# Prediction
+index = np.argmax(model.predict(np.array([test])))
+
+print("\n")
+print("\nLe mot retranscrit est ---> " + (MOTS[index]).upper() + " <---")
+print("Si ce n'est pas la bonne retranscription, veuillez revérifier le fichier audio, l'erreur vient probablement de "
+      "l'enregistrement ")
+print("\n")
 
